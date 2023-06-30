692209e4f84588e84be1d432bedb710869476b1d
Hafizi bin Ismail <hafizi.ismail@infinitilab.com>
2023-06-30 21:41:11 +0800

    Added audience growth rate to dashboard

--- dashboard.py
+++ dashboard.py
@@ -3,6 +3,9 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import plotly.express as px
 
+import os 
+import re
+
 data = pd.read_csv('data_with_metrics.csv')
 
 st.title("Satire News Website Social Media Monitor")
@@ -91,3 +94,90 @@ fig.update_layout(
 )
 
 st.plotly_chart(fig)
+
+def load_all_historical_data_csv():
+    dataframes = []
+    pattern = r"Historical30d_(\w+)\.csv"
+    for filename in os.listdir("data"):
+        match = re.search(pattern, filename)
+        if match:
+            profile = match.group(1)
+            filepath = os.path.join("data", filename)
+            df = pd.read_csv(filepath)
+            df['username'] = profile  # Add a 'Profile' column to identify the data source
+            dataframes.append(df)
+
+
+    combined_df = pd.concat(dataframes, ignore_index=True)
+    combined_df = combined_df[['username'] + list(combined_df.columns[:-1])]
+
+    # somehow, extra spacing there
+    combined_df['date'] = pd.to_datetime(combined_df['date'])
+    combined_df['followers'] = combined_df[' followers']
+    combined_df['following'] = combined_df[' following']
+    combined_df['tweets'] = combined_df[' tweets']
+
+    # net change
+    combined_df['net_change_followers'] = combined_df.groupby('username')['followers'].diff()
+    # growth rate
+    combined_df['growth_rate_followers'] = combined_df.groupby('username')['followers'].pct_change() * 100  # Calculate the net change
+    
+    combined_df.drop([' followers', ' following', ' tweets'], axis=1, inplace=True)
+
+    return combined_df
+
+historical_data = load_all_historical_data_csv()
+
+filtered_data3 = historical_data[historical_data['username'].isin(selected_name)]
+
+
+# Audience Growth Rate
+st.header("Audience Growth Rate")
+st.text("Audience Growth Rate = ((Ending Followers - Starting Followers) / Starting Followers) * 100")
+
+# Followers count over time
+st.subheader("Followers count over time")
+
+fig = px.line(filtered_data3, x='date', y='followers', color='username')
+
+st.plotly_chart(fig)
+
+# Net change of followers over time --------------------
+st.subheader("Followers net changes over time")
+
+fig = px.line(filtered_data3, x='date', y='net_change_followers', color='username')
+
+st.plotly_chart(fig)
+# ------------------------------------------------------
+
+# Daily Growth Rate ------------------------------------
+st.subheader("Daily growth rate")
+
+fig = px.line(filtered_data3, x='date', y='growth_rate_followers', color='username')
+
+st.plotly_chart(fig)
+
+# Audience Growth Rate in a month
+st.subheader("Audience Growth Rate in the time period")
+
+def get_total_growth_rate_follower(df_combined):
+    usernames = df_combined['username'].unique()
+    df = pd.DataFrame(usernames, columns=['username'])
+    total_growth_rates = {}
+    for username in usernames:
+        user_df = df_combined[df_combined['username'] == username]
+        followers_start = user_df.iloc[0]['followers']
+        followers_end = user_df.iloc[-1]['followers']
+        total_growth_rate = ((followers_end - followers_start) / followers_start) * 100
+        total_growth_rates[username] = total_growth_rate
+    return total_growth_rates
+
+list_total_growth_rate = get_total_growth_rate_follower(historical_data)
+
+start_date = historical_data.iloc[0]['date']
+end_date = historical_data.iloc[-1]['date']
+
+st.subheader(f"From {start_date.strftime('%Y-%m-%d')} until {end_date.strftime('%Y-%m-%d')}")
+
+for username, growth_rate in list_total_growth_rate.items():
+    st.text(f"{username}: {growth_rate:.2f}%")
